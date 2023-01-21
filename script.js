ab8bd977eb8aaa1ca513b3b9f96b0aab79bd0c12
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-01-20 22:12:51 -0400

    aula 4 concluida com sucesso
    aprendi muito sobre o localstorage do navegador

--- script.js
+++ script.js
@@ -1,19 +1,72 @@
+
+//registrar em memoria
 const form = document.querySelector('#form-habits');
+
+//registrar em memoria o objeto NLWSetup
+//da biblioteca do nlw disponibilizou
+//está sendo importada atraves do link no HTML, que busca no git
 const nlwSetup = new NLWSetup(form);
 
-const data = {
-    baska: ['02-01', '08-01'],
-    agua:   ['01-01', '05-01', '07-01', '08-01'],
-    comer:  ['01-01', '02-01', '04-01', '05-01'],
-    codar: ['01-01', '02-01', '03-01', '04-01', '05-01', '05-01', '06-01'],  
-    jogar: ['01-01', '02-01', '04-01', '07-01'],
-    assistir: ['02-01', '06-01'],
-    ler: ['05-01', '03-01'],
-    namorar: ['01-01', '02-01', '03-01', '05-01', '06-01', '07-01', '08-01']
+//registrar em memoria
+const button = document.querySelector('header button');
+
+
+        /*adiconar ouvidor de eventos = sempre que um evento x aconotecer, execute uma função
+        sintaxe: addEventListener('evento', função) */
+button.addEventListener('click', add);
+
+form.addEventListener('change', save);
+
+//adicona o data(atual) no nlwSetup -> que é um objeto
+function add(){
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+    //const today = '18/01'
+                    //func verifica se a data já n existe
+    const dayExists = nlwSetup.dayExists(today);
+
+    if(dayExists){
+        alert('Dia já incluso ❌');
+        return;
+    }
+
+    //alert('Dia adicionado com sucesso ✅')
+    //salva no objeto a data atual
+    nlwSetup.addDay(today);
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+function save(){
+    //converte a data atual (formato de objeto) em string
+    //depois salva no local storage do browse com a chave 'habits'
+    localStorage.setItem('habits', JSON.stringify(nlwSetup.data));
+}
+
+/*
+JSON.stringify(objeto)) = transforma em string o objeto
+JSON.parse(string) = transforma em objeto a string
+*/
+
+
+const data = JSON.parse(localStorage.getItem('habits')) || {}; 
+
+nlwSetup.setData(data);
+nlwSetup.load();
+
+
+/* formato
+const data = {
+    baska: [],
+    agua:   [],
+    comer:  [],
+    codar: [],  
+    jogar: [],
+    assistir: [],
+    ler: [],
+    namorar: []
+}
+
+*/
+
+
 
 
 
